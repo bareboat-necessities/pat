44c2caeddccbe560fc90ad7797550632deffee1e
mgrouch <mgrushinskiy@gmail.com>
2022-08-08 17:38:02 -0400

    circleci

--- build-deb.sh
+++ build-deb.sh
@@ -8,8 +8,8 @@ apt-get update  -y -q
 apt-get install -y -q wget xz-utils gnupg ca-certificates
 apt-get upgrade -y -q
 
-apt-get -y install devscripts/bullseye-backports debhelper-compat/bullseye-backports \
-  build-essential/bullseye-backports lintian \
+apt-get -y install devscripts/bullseye-backports debhelper-compat \
+  build-essential lintian \
   golang/bullseye-backports golang-go/bullseye-backports \
   golang-doc/bullseye-backports golang-src/bullseye-backports \
   libax25 libax25-dev
